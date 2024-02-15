26d49b17e006809a427b4af2626b926a4d8269dc
TantoFaz <thiago.playgames15@gmail.com>
2024-02-14 22:46:35 -0300

    Adicionando métodos de parsing no objeto de `PageSet` e adicioando pré processador de URLs

--- src/page-set.ts
+++ src/page-set.ts
@@ -1,5 +1,35 @@
-import Page from "./page"
+import Page, { Item, Model, Extractor } from "./page"
 
-class PageSet extends Array<Page> {}
+class PageSet extends Array<Page> {
+    public parseItemGroupForAll(
+        query: string,
+        model: Model,
+        limit?: number
+    ): Item[] {
+        const dataSet: Item[] = []
+
+        for (const page of this) {
+            const adjustedLimit = limit !== undefined && limit !== null ? limit - this.length : limit
+            const currentDataSet = page.parseItemGroup(query, model, adjustedLimit)
+            dataSet.push(...currentDataSet)
+        }
+
+        return dataSet
+    }
+
+    public parseAll(query: string, extractor: Extractor, limit?: number): string[] {
+        const dataSet: string[] = []
+
+        for (const page of this) {
+            if (limit !== undefined && limit !== null && this.length >= limit) break
+
+            const adjustedLimit = limit !== undefined && limit !== null ? limit - this.length : limit
+            const currentDataSet = page.parseAll(query, extractor, adjustedLimit)
+            dataSet.push(...currentDataSet)
+        }
+
+        return dataSet
+    }
+}
 
 export default PageSet
---
+++ src/utils.ts
@@ -0,0 +1,18 @@
+type DataItem = {
+    [key: string]: any
+}
+
+function preProcessUrls(urlTemplate: string, data: DataItem[]): string[] {
+    const urls: string[] = []
+
+    for (const dataItem of data) {
+        const url = urlTemplate.replace(/\{([^}]+)\}/g, (match, key) => dataItem[key])
+        urls.push(url)
+    }
+
+    return urls
+}
+
+export {
+    preProcessUrls
+}
